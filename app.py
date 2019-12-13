ac2126ca9d0c59bdba56d563acaf0a22591d4ad0
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-12-12 17:00:53 -0800

    error fixing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractorProject')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor2')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 items = db.items
